5c2d5eb0c9478806f0d09ce34b686b6461bb05ce
Jesse Ryan <hello@jesseryan-dev.com>
2020-12-27 14:56:22 -0800

    everything from initial project to have button and movement controlls.

---
+++ Assets/Sphere.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    // Start is called before the first frame update
+    protected Joystick joystick;
+    protected FixedJoybutton fixedJoybutton;
+
+
+    protected bool jump;
+
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        fixedJoybutton = FindObjectOfType<FixedJoybutton>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = new Vector3(joystick.Horizontal * 100F,
+                                rigidbody.velocity.y,
+                                joystick.Vertical * 100F);
+
+        if(!jump && fixedJoybutton.IsPressed() ){
+            jump = true;
+            rigidbody.velocity += Vector3.up * 10F;
+        }
+
+        if (jump && !fixedJoybutton.IsPressed()) {
+            jump = false;
+        }
+    }
+}
